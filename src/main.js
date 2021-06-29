ffda79183ac2e952b8adb7bd08354a08166803e5
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-29 15:52:05 -0500

    have limited number of gifs
    Co-Authored-By: Allister <allisterkmoon@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-function doThing () {
-  $("#gifSearch").click(function(){
-    const userInput = $("#gif").val();
-    return fetch(`https://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${process.env.API_KEY}`)
+$("#gifSearch").click(function() {
+  const userInput = $("#gif").val();
+
+  const url = `https://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${process.env.API_KEY}&limit=15`;
+
+  return fetch(url)
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
@@ -15,23 +17,16 @@ function doThing () {
 
       return response.json();
     })
-    .then(function (response) {      
-      for (let i=0; i < response.data.length; i++){
-        console.log(response.data[i].images.original.url);
+    .then(function (response) {
+      for (let item of response.data) {
+        const { url } = item.images.original;
+        
+        console.log(url);
+        $('.image-dump').append(`<img src="${url}">`)
       }
-      let i = response.data[0].images.original.url
-      $("#image1").text(i)
-      $("a").attr("href",i)
-      $("img").attr("src",i)
-      console.log(response);
+
     })
     .catch(function(error) {
       return error;
     });
-    
-  })
-}
-
-
-
-doThing();
+});
